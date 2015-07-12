9773d63d40f4da95ad4a3bc303c45b93675e29b0
Candy <candy@birdy-4.local>
2015-07-12 16:39:38 +0200

    second

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,15 +1,11 @@
 numbers = range(100)
 
 for number in numbers:
-
 	if number % 3 == 0 and number % 5 == 0:
-		print "fizzbuzz"
-		
+		print "fizzbuzz"	
 	elif number % 3 == 0:
 		print "fizz"
-
 	elif number % 5 == 0:
 		print "buzz"
-
 	else:
 		print " %d " % number
